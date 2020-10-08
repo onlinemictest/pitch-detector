5eb3cf6f09dc31ba2189ceca5cff90cfa20e0af4
Florian Klampfer <mail@qwtel.com>
2020-10-08 14:17:34 +0700

    Setup toolchain

---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import typescript from 'rollup-plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
+
+export default {
+    input: 'src/index.ts',
+    output: {
+        file: `dist/index.js`,
+        format: 'es',
+        sourcemap: true
+    },
+    plugins: [
+        typescript(),
+        resolve(),
+        terser(),
+        commonjs(),
+    ],
+};
